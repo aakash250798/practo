23edb4ae550fd30333a8d125fb70ad86ca1a093d
akash <aakash250798@gmail.com>
2026-02-08 16:04:55 +0530

    Few changes

--- src/main/java/com/my/firstProject/practo/entity/Doctor.java
+++ src/main/java/com/my/firstProject/practo/entity/Doctor.java
@@ -1,11 +1,14 @@
 package com.my.firstProject.practo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -27,8 +30,29 @@ public class Doctor {
     private String location;
     private Integer fees;
     private Double experience;
-    private List<LocalTime> timeSlots; //LocalTime time = LocalTime.of(10, 30, 15);
+    private String shift;
+    private List<Integer> timeSlots; //LocalTime time = LocalTime.of(10, 30, 15);
     @OneToMany
+    @JsonIgnore
     private List<Appointment> appointment;
 
+    public void setShift(String shift) {
+       if(shift.equals("Morning")){
+           this.setTimeSlots(Arrays.asList(900, 930, 1000, 1030, 1100, 1130, 1200));
+       }
+       else if(shift.equals("Afternoon")){
+           this.setTimeSlots(Arrays.asList(1200, 1230, 1300, 1330, 1400, 1430, 1500));
+       }
+
+       else if(shift.equals("Evening")){
+           this.setTimeSlots(Arrays.asList(1500, 1530, 1600, 1630, 1700, 1730,1800));
+       }
+
+       else if(shift.equals("Night")){
+           this.setTimeSlots(Arrays.asList(1800, 1830,1900, 1930, 2000, 2030, 2100));
+       }
+
+       else
+           this.setTimeSlots(new ArrayList<>());
+    }
 }
